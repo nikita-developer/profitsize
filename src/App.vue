ca28d6cbc0dd957b737b3afbebc7bffa1e6f9d76
Nikita <nikita.kuznetsov.r@gmail.com>
2022-09-14 13:49:23 +0300

    Добавил preloader

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
 	<div class="page">
+		<transition name="fade">
+			<Preloader v-if="isPreloader"></Preloader>
+		</transition>
 		<Todo class="page__todo"></Todo>
 	</div>
 </template>
 <script>
 	import Todo from './components/Todo/Todo.vue';
+	import Preloader from './components/Preloader/Preloader.vue';
 	export default {
+		data() {
+			return {
+				isPreloader: true
+			}
+		},
 		components: {
-			Todo
+			Todo,
+			Preloader
+		},
+		methods: {
+			preloader() {
+				setTimeout(() => {
+					this.isPreloader = false
+				}, 3000)
+			}
+		},
+		mounted() {
+			this.preloader()
 		}
 	}
 </script>
---
+++ src/components/Preloader/Preloader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="preloader">
+        <div class="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import 'styles/main';
+</style>
